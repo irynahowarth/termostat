3eae5c0db00ca08a6dfbd77926a5229e76965e0d
Reiss Johnson <reissjohnson@me.com>
2016-02-17 15:33:01 +0000

    implimented thermostat up button

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -3,11 +3,21 @@ describe('thermostat', function(){
   beforeEach(function(){
     thermostat = new Thermostat();
   });
+
   describe('thermostat starts at 20 degrees', function(){
     it('starts at 20 degrees', function(){
       expect(thermostat.state).toEqual(20);
     });
   });
+
+  describe('thermostat can increase with up button', function(){
+    it('increases the state', function(){
+      thermostat.up();
+      expect(thermostat.state).toEqual(21);
+    });
+  });
+
+
 
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,4 +1,5 @@
 'use strict';
 
 function Thermostat(){this.state = 20;}
+  Thermostat.prototype.up = function(){ return this.state += 1;};
 
